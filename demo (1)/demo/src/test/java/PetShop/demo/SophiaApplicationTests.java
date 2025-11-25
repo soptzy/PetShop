1e4a85c868e13fe8c2c8e93b9539df2ece615436
soptzy <161872579+soptzy@users.noreply.github.com>
2025-11-25 08:51:23 -0300

    Add files via upload

---
+++ demo (1)/demo/src/test/java/PetShop/demo/SophiaApplicationTests.java
@@ -0,0 +1,13 @@
+package PetShop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SophiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
